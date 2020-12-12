96b5b38400bad32a19c52f53210fb5fa6532d2bd
Juan Carlos Concha <jnkarlos.c@gmail.com>
2020-12-12 03:42:13 -0300

    added when transaction created refresh view #6

--- lib/monobanco_web/live/transaction_component.ex
+++ lib/monobanco_web/live/transaction_component.ex
@@ -1,3 +1,19 @@
 defmodule TransactionComponent do
   use Phoenix.LiveComponent
+
+
+  def class_deposit(transaction) do
+    if transaction.is_deposit do
+      "has-text-success"
+    else
+      "has-text-danger"
+    end
+  end
+
+  def format_date(transaction) do
+    date = "#{transaction. inserted_at.year}/#{transaction. inserted_at.month}/#{transaction. inserted_at.day}"
+    time = "#{transaction. inserted_at.hour}:#{transaction. inserted_at.minute}"
+
+    date <> " " <> time
+  end
 end
